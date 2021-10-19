7942c240669a9a59f6f3d7f772b1aabdc0da6dbb
Aviril <89867985+Aviril@users.noreply.github.com>
2021-10-19 16:22:52 +0300

    Crafted example weapon asset template

--- [Library]/assetify_library/handlers/loader/weapon.lua
+++ [Library]/assetify_library/handlers/loader/weapon.lua
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------
 --[[ Resource: Assetify Library
-     Script: handlers: weaponLoader.lua
+     Script: handlers: loader: weapon.lua
      Server: -
      Author: OvileAmriam
      Developer: Aviril
@@ -13,7 +13,7 @@
 --[[ Variables ]]--
 -------------------
 
-local loadedClientWeapons = {
+local createdElements = {
     modelIDs = {},
     models = {txd = {}, dff = {}, col = {}}
 }
@@ -25,9 +25,9 @@ local loadedClientWeapons = {
 
 function _getWeaponID(weaponType)
 
-    if not weaponType or not serverWeapons[weaponType] or not loadedClientWeapons.modelIDs[weaponType] then return false end
+    if not weaponType or not serverWeapons[weaponType] or not createdElements.modelIDs[weaponType] then return false end
 
-    return loadedClientWeapons.modelIDs[weaponType]
+    return createdElements.modelIDs[weaponType]
 
 end
 
@@ -46,30 +46,30 @@ addEventHandler("onClientResourceStart", resource, function()
             local weaponDFFPath = ":mod_loader/files/weapons/"..weaponType.."/weapon.dff"
             local weaponCOLPath = ":mod_loader/files/weapons/"..weaponType.."/weapon.col"
             if weaponTXDPath and File.exists(weaponTXDPath) then
-                if not loadedClientWeapons.models.txd[weaponTXDPath] then
-                    loadedClientWeapons.models.txd[weaponTXDPath] = EngineTXD(weaponTXDPath)
+                if not createdElements.models.txd[weaponTXDPath] then
+                    createdElements.models.txd[weaponTXDPath] = EngineTXD(weaponTXDPath)
                 end
-                if loadedClientWeapons.models.txd[weaponTXDPath] and isElement(loadedClientWeapons.models.txd[weaponTXDPath]) then
-                    loadedClientWeapons.models.txd[weaponTXDPath]:import(generatedModelID)
+                if createdElements.models.txd[weaponTXDPath] and isElement(createdElements.models.txd[weaponTXDPath]) then
+                    createdElements.models.txd[weaponTXDPath]:import(generatedModelID)
                 end
             end
             if weaponDFFPath and File.exists(weaponDFFPath) then
-                if not loadedClientWeapons.models.dff[weaponDFFPath] then
-                    loadedClientWeapons.models.dff[weaponDFFPath] = EngineDFF(weaponDFFPath)
+                if not createdElements.models.dff[weaponDFFPath] then
+                    createdElements.models.dff[weaponDFFPath] = EngineDFF(weaponDFFPath)
                 end
-                if loadedClientWeapons.models.dff[weaponDFFPath] and isElement(loadedClientWeapons.models.dff[weaponDFFPath]) then
-                    loadedClientWeapons.models.dff[weaponDFFPath]:replace(generatedModelID)
+                if createdElements.models.dff[weaponDFFPath] and isElement(createdElements.models.dff[weaponDFFPath]) then
+                    createdElements.models.dff[weaponDFFPath]:replace(generatedModelID)
                 end
             end
             if weaponCOLPath and File.exists(weaponCOLPath) then
-                if not loadedClientWeapons.models.col[weaponCOLPath] then
-                    loadedClientWeapons.models.col[weaponCOLPath] = EngineCOL(weaponCOLPath)
+                if not createdElements.models.col[weaponCOLPath] then
+                    createdElements.models.col[weaponCOLPath] = EngineCOL(weaponCOLPath)
                 end
-                if loadedClientWeapons.models.col[weaponCOLPath] and isElement(loadedClientWeapons.models.col[weaponCOLPath]) then
-                    loadedClientWeapons.models.col[weaponCOLPath]:replace(generatedModelID)
+                if createdElements.models.col[weaponCOLPath] and isElement(createdElements.models.col[weaponCOLPath]) then
+                    createdElements.models.col[weaponCOLPath]:replace(generatedModelID)
                 end
             end
-            loadedClientWeapons.modelIDs[weaponType] = generatedModelID
+            createdElements.modelIDs[weaponType] = generatedModelID
         end
     end
 
