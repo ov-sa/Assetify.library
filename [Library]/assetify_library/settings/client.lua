418865ad3306895e65be68ed68995a71e9b141e4
Aviril <89867985+Aviril@users.noreply.github.com>
2022-05-03 00:59:10 +0300

    Create client.lua

---
+++ [Library]/assetify_library/settings/client.lua
@@ -0,0 +1,18 @@
+----------------------------------------------------------------
+--[[ Resource: Assetify Library
+     Script: settings: client.lua
+     Author: vStudio
+     Developer(s): Aviril, Tron
+     DOC: 19/10/2021
+     Desc: Client Sided Settings ]]--
+----------------------------------------------------------------
+
+
+------------------
+--[[ Settings ]]--
+------------------
+
+GTAWorldSettings = {
+    removeWorld = true,
+    waterLevel = 0.01
+}
