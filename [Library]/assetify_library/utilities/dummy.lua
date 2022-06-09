07ce790823aeba43a306e9f9c29dad0d2dd35346
Aviril <89867985+Aviril@users.noreply.github.com>
2022-06-10 00:14:21 +0300

    Update dummy.lua

--- [Library]/assetify_library/utilities/dummy.lua
+++ [Library]/assetify_library/utilities/dummy.lua
@@ -18,11 +18,12 @@ local imports = {
     destroyElement = destroyElement,
     setmetatable = setmetatable,
     createObject = createObject,
+    createPed = createPed,
+    createVehicle = createVehicle,
     setElementAlpha = setElementAlpha,
     setElementDoubleSided = setElementDoubleSided,
     setElementDimension = setElementDimension,
-    setElementInterior = setElementInterior,
-    triggerEvent = triggerEvent
+    setElementInterior = setElementInterior
 }
 
 
@@ -61,18 +62,34 @@ function dummy:load(assetType, assetName, assetClump, clumpMaps, dummyData)
     local cAsset, cData = manager:getData(assetType, assetName, syncer.librarySerial)
     cData = (cAsset and cData and ((cAsset.manifestData.assetClumps and cAsset.unSynced.assetCache[assetClump].cAsset.synced) or cData)) or false
     if not cAsset or not cData then return false end
+    local dummyType = availableAssetPacks[assetType].assetType
+    if not dummyType then return false end
     dummyData.position, dummyData.rotation = dummyData.position or {}, dummyData.rotation or {}
     dummyData.position.x, dummyData.position.y, dummyData.position.z = imports.tonumber(dummyData.position.x) or 0, imports.tonumber(dummyData.position.y) or 0, imports.tonumber(dummyData.position.z) or 0
     dummyData.rotation.x, dummyData.rotation.y, dummyData.rotation.z = imports.tonumber(dummyData.rotation.x) or 0, imports.tonumber(dummyData.rotation.y) or 0, imports.tonumber(dummyData.rotation.z) or 0
     self.assetType, self.assetName = assetType, assetName
-    self.cModelInstance = imports.createObject(cData.modelID, dummyData.position.x, dummyData.position.y, dummyData.position.z, dummyData.rotation.x, dummyData.rotation.y, dummyData.rotation.z)
     self.syncRate = imports.tonumber(dummyData.syncRate)
-    imports.setElementDoubleSided(self.cModelInstance, true)
+    if dummyType == "object" then
+        self.cModelInstance = imports.createObject(cData.modelID, dummyData.position.x, dummyData.position.y, dummyData.position.z, dummyData.rotation.x, dummyData.rotation.y, dummyData.rotation.z)
+        imports.setElementDoubleSided(self.cModelInstance, true)
+        network:emit("Assetify:onRecieveElementModel", false, self.cModelInstance, assetType, assetName, assetClump, clumpMaps)
+        if cData.collisionID then
+            self.cCollisionInstance = imports.createObject(cData.collisionID, dummyData.position.x, dummyData.position.y, dummyData.position.z, dummyData.rotation.x, dummyData.rotation.y, dummyData.rotation.z)
+        end
+    elseif dummyType == "ped" then
+        self.cModelInstance = imports.createPed(cData.modelID, dummyData.position.x, dummyData.position.y, dummyData.position.z, dummyData.rotation.z)
+        if cData.collisionID then
+            self.cCollisionInstance = imports.createPed(cData.collisionID, dummyData.position.x, dummyData.position.y, dummyData.position.z, dummyData.rotation.z)
+        end
+    elseif dummyType == "vehicle" then
+        self.cModelInstance = imports.createVehicle(cData.modelID, dummyData.position.x, dummyData.position.y, dummyData.position.z, dummyData.rotation.x, dummyData.rotation.y, dummyData.rotation.z)
+        if cData.collisionID then
+            self.cCollisionInstance = imports.createVehicle(cData.collisionID, dummyData.position.x, dummyData.position.y, dummyData.position.z, dummyData.rotation.x, dummyData.rotation.y, dummyData.rotation.z)
+        end
+    end
     imports.setElementDimension(self.cModelInstance, imports.tonumber(dummyData.dimension) or 0)
     imports.setElementInterior(self.cModelInstance, imports.tonumber(dummyData.interior) or 0)
-    imports.triggerEvent("Assetify:onRecieveElementModel", localPlayer, self.cModelInstance, assetType, assetName, assetClump, clumpMaps)
-    if cData.collisionID then
-        self.cCollisionInstance = imports.createObject(cData.collisionID, dummyData.position.x, dummyData.position.y, dummyData.position.z, dummyData.rotation.x, dummyData.rotation.y, dummyData.rotation.z)
+    if self.cCollisionInstance then
         imports.setElementAlpha(self.cCollisionInstance, 0)
         self.cStreamer = streamer:create(self.cModelInstance, "dummy", {self.cCollisionInstance}, self.syncRate)
     end
