c9386fe110d6062c1e840c180c4f27f033ed6fdd
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-07-21 15:50:23 +0300

    Made handler safe and more enhanced as per @Aviril 's review.

--- [Library]/assetify_library/handlers/cli/updater.lua
+++ [Library]/assetify_library/handlers/cli/updater.lua
@@ -14,6 +14,7 @@
 
 local cli = cli:import()
 local imports = {
+    pairs = pairs,
     collectgarbage = collectgarbage,
     fetchRemote = fetchRemote,
     restartResource = restartResource,
@@ -30,16 +31,27 @@ local updateResources = nil
 updateResources = {
     updateTags = {"file", "script"},
     fetchSource = function(base, version, ...) return (base and version and string.format(base, version, ...)) or false end,
-    onUpdateCallback = function(isCompleted)
+    onUpdateCallback = function(isCompleted, isNotification)
         if isCompleted then
             syncer.libraryVersion = updateResources.updateCache.libraryVersion
+            for i, j in imports.pairs(updateResources.updateCache.backup) do
+                imports.outputDebugString("[Assetify] | Backed up <"..i.."> due to compatibility breaking changes; Kindly acknowledge it accordingly!", 3)
+                file:write(i, j)
+            end
+            for i, j in imports.pairs(updateResources.updateCache.output) do
+                file:write(i, j)
+            end
+            imports.outputDebugString("[Assetify] | Update successfully completed; Rebooting!", 3)
             for i = 1, #updateResources, 1 do
                 local j = updateResources[i]
                 local cResource = imports.getResourceFromName(j.resourceName)
                 if cResource then imports.restartResource(cResource) end
             end
         end
+        if isNotification and not isCompleted then imports.outputDebugString("[Assetify] | Update failed due to connectivity issues; Try again later...", 3) end
+        if updateResources.updateThread then updateResources.updateThread:destroy() end
         updateResources.updateCache = nil
+        updateResources.updateThread = nil
         cli.public.isLibraryBeingUpdated = nil
         imports.collectgarbage()
         return true
@@ -64,10 +76,6 @@ end
 -----------------------
 
 function cli.private:update(resourcePointer, responsePointer, isUpdateStatus)
-    if isUpdateStatus ~= nil then
-        imports.outputDebugString("[Assetify] | "..((isUpdateStatus and "Update successfully completed; Rebooting!") or "Update failed due to connectivity issues; Try again later..."), 3)
-        return updateResources.onUpdateCallback(isUpdateStatus)
-    end
     if not responsePointer then
         updateResources.updateThread = thread:create(function()
             for i = 1, #updateResources, 1 do
@@ -75,7 +83,7 @@ function cli.private:update(resourcePointer, responsePointer, isUpdateStatus)
                 local resourceMeta = updateResources.updateCache.libraryVersionSource..(resourcePointer.resourceName).."/meta.xml"
                 imports.fetchRemote(resourceMeta, function(...) resoureResponse = table.pack(...); updateResources.updateThread:resume() end)
                 updateResources.updateThread:pause()
-                if not resoureResponse[1] or not resoureResponse[2] or (resoureResponse[2] ~= 0) then return cli.private:update(_, _, false) end
+                if not resoureResponse[1] or not resoureResponse[2] or (resoureResponse[2] ~= 0) then return updateResources.onUpdateCallback(false, true) end
                 local isLastIndex = false
                 for i = 1, #updateResources.updateTags, 1 do
                     for j in string.gmatch(resoureResponse[1], "<".. updateResources.updateTags[i].." src=\"(.-)\"(.-)/>") do
@@ -93,26 +101,20 @@ function cli.private:update(resourcePointer, responsePointer, isUpdateStatus)
                 isLastIndex = true
                 cli.private:update(resourcePointer, {resourceMeta, "meta.xml", resoureResponse[1]})
             end
-            cli.private:update(_, _, true)
+            updateResources.onUpdateCallback(true, true)
         end)
         updateResources.updateThread:resume()
     else
         local isBackupToBeCreated = (resourcePointer.resourceBackup and resourcePointer.resourceBackup[(responsePointer[2])] and true) or false
         responsePointer[2] = resourcePointer.resourcePointer..responsePointer[2]
-        if isBackupToBeCreated then imports.outputDebugString("[Assetify] | Backed up <"..responsePointer[2].."> due to compatibility breaking changes; Kindly update it accordingly!", 3) end
+        if isBackupToBeCreated then updateResources.updateCache.backup[(responsePointer[2]..".backup")] = file:read(responsePointer[2]) end
         if responsePointer[3] then
-            if isBackupToBeCreated then file:write(responsePointer[2]..".backup", file:read(responsePointer[2])) end
-            file:write(responsePointer[2], responsePointer[3])
+            updateResources.updateCache.output[(responsePointer[2])] = responsePointer[3]
             updateResources.updateThread:resume()
         else
             imports.fetchRemote(responsePointer[1], function(response, status)
-                --TODO: INSTEAD OF DESTROYING HANDLE IN THIS SOME HANDLER
-                if not response or not status or (status ~= 0) then
-                    cli.private:update(_, _, false)
-                    return updateResources.updateThread:destroy()
-                end
-                if isBackupToBeCreated then file:write(responsePointer[2]..".backup", file:read(responsePointer[2])) end
-                file:write(responsePointer[2], response)
+                if not response or not status or (status ~= 0) then return updateResources.onUpdateCallback(false, true) end
+                updateResources.updateCache.output[(responsePointer[2])] = response
                 updateResources.updateThread:resume()
             end)
         end
@@ -125,9 +127,9 @@ function cli.public:update(isAction)
     cli.public.isLibraryBeingUpdated = true
     if isAction then imports.outputDebugString("[Assetify] | Fetching latest version; Hold up...", 3) end
     imports.fetchRemote(syncer.librarySource, function(response, status)
-        if not response or not status or (status ~= 0) then return updateResources.onUpdateCallback() end
+        if not response or not status or (status ~= 0) then return updateResources.onUpdateCallback(false, true) end
         response = table.decode(response)
-        if not response or not response.tag_name then return updateResources.onUpdateCallback() end
+        if not response or not response.tag_name then return updateResources.onUpdateCallback(false, true) end
         if syncer.libraryVersion == response.tag_name then
             if isAction then imports.outputDebugString("[Assetify] | Already upto date - "..response.tag_name, 3) end
             return updateResources.onUpdateCallback()
@@ -136,6 +138,7 @@ function cli.public:update(isAction)
         imports.outputDebugString("[Assetify] | "..((isToBeUpdated and not isAutoUpdate and "Updating to latest version") or (isToBeUpdated and isAutoUpdate and "Auto-updating to latest version") or "Latest version available").." - "..response.tag_name, 3)
         if not isToBeUpdated then return updateResources.onUpdateCallback() end
         updateResources.updateCache = {
+            output = {}, backup = {},
             isAutoUpdate = isAutoUpdate,
             libraryVersion = response.tag_name,
             libraryVersionSource = updateResources.fetchSource(updateResources[1].resourceSource, response.tag_name),
