f66c2812c7398d627988c5c7efe451724c22f6cf
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-11-24 15:48:08 +0300

    Init resource class

--- [Library]/assetify_library/utilities/engine/resource.lua
+++ [Library]/assetify_library/utilities/engine/resource.lua
@@ -20,3 +20,30 @@ local imports = {}
 -------------------------
 
 local resource = class:create("resource")
+
+function resource.public:create(...)
+    local cResource = self:createInstance()
+    if cResource and not cResource:load(...) then
+        cResource:destroyInstance()
+        return false
+    end
+    return cResource
+end
+
+function resource.public:destroy(...)
+    if not resource.public:isInstance(self) then return false end
+    return self:unload(...)
+end
+
+function resource.public:load()
+    if not resource.public:isInstance(self) then return false end
+    resource.public.buffer.resource[self] = true
+    return true
+end
+
+function resource.public:unload()
+    if not resource.public:isInstance(self) then return false end
+    resource.public.buffer.resource[self] = nil
+    self:destroyInstance()
+    return true
+end
