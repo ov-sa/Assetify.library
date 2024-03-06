27246317b41f04a5127c62ba6ab1d14a999c6cb9
Tron <89867985+Aviril@users.noreply.github.com>
2024-03-07 01:12:49 +0300

    Simplified sound & anim APIs

---
+++ [Library]/assetify_library/handlers/api/sound/api.lua
@@ -0,0 +1,54 @@
+----------------------------------------------------------------
+--[[ Resource: Assetify Library
+     Script: handlers: api: sound: api.lua
+     Author: vStudio
+     Developer(s): Aviril, Tron, Mario, Аниса
+     DOC: 19/10/2021
+     Desc: Sound APIs ]]--
+----------------------------------------------------------------
+
+
+-----------------
+--[[ Imports ]]--
+-----------------
+
+local imports = {
+    playSound = playSound,
+    playSound3D = playSound3D,
+    setSoundVolume = setSoundVolume
+}
+
+
+---------------------
+--[[ APIs: Sound ]]--
+---------------------
+
+if localPlayer then
+    function manager.API.Sound.playSound(assetName, soundCategory, soundIndex, soundVolume, isScoped, ...)
+        if not syncer.isLibraryLoaded then return false end
+        local cAsset, isLoaded = manager:getAssetData("sound", assetName, syncer.librarySerial)
+        if not cAsset or not isLoaded then return false end
+        if not cAsset.manifestData.assetSounds or not cAsset.unSynced.assetCache[soundCategory] or not cAsset.unSynced.assetCache[soundCategory][soundIndex] or not cAsset.unSynced.assetCache[soundCategory][soundIndex].cAsset then return false end
+        local cSound = imports.playSound(cAsset.unSynced.rwCache.sound[(cAsset.unSynced.assetCache[soundCategory][soundIndex].cAsset.rwPaths.sound)], ...)
+        if cSound then
+            if soundVolume then imports.setSoundVolume(cSound, soundVolume) end
+            if isScoped then manager:setElementScoped(cSound) end
+        end
+        return cSound
+    end
+
+    function manager.API.Sound.playSound3D(assetName, soundCategory, soundIndex, soundVolume, isScoped, ...)
+        if not syncer.isLibraryLoaded then return false end
+        local cAsset, isLoaded = manager:getAssetData("sound", assetName, syncer.librarySerial)
+        if not cAsset or not isLoaded then return false end
+        if not cAsset.manifestData.assetSounds or not cAsset.unSynced.assetCache[soundCategory] or not cAsset.unSynced.assetCache[soundCategory][soundIndex] or not cAsset.unSynced.assetCache[soundCategory][soundIndex].cAsset then return false end
+        local cSound = imports.playSound3D(cAsset.unSynced.rwCache.sound[(cAsset.unSynced.assetCache[soundCategory][soundIndex].cAsset.rwPaths.sound)], ...)
+        if cSound then
+            if soundVolume then imports.setSoundVolume(cSound, soundVolume) end
+            if isScoped then manager:setElementScoped(cSound) end
+        end
+        return cSound
+    end
+else
+
+end
