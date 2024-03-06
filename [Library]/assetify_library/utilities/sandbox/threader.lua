9af4111f762f3f14c68324338d708d1042187787
Tron <89867985+Aviril@users.noreply.github.com>
2024-03-06 22:00:16 +0300

    .

--- [Library]/assetify_library/utilities/sandbox/threader.lua
+++ [Library]/assetify_library/utilities/sandbox/threader.lua
@@ -91,9 +91,7 @@ function thread.public:createPromise(callback, config)
     thread.private.promises[cPromise] = {}
     if not config.isAsync then execFunction(callback, cPromise.resolve, cPromise.reject)
     else thread.public:create(function(self) execFunction(callback, self, cPromise.resolve, cPromise.reject) end):resume() end
-    if config.timeout then
-        cTimer = timer:create(function() cPromise.reject("Promise - Timed Out") end, config.timeout, 1)
-    end
+    if config.timeout then cTimer = timer:create(function() cPromise.reject("Promise - Timed Out") end, config.timeout, 1) end
     return cPromise
 end
 
