37e41915c8e516aa164dd1a42c14343fda23809b
Tron <89867985+Aviril@users.noreply.github.com>
2024-11-29 06:34:10 +0300

    Added `headers` and `credentials` support to `rest.get(...)` & `rest.post(...)`

--- [Library]/assetify_library/utilities/sandbox/rest.lua
+++ [Library]/assetify_library/utilities/sandbox/rest.lua
@@ -25,25 +25,27 @@ local imports = {
 
 local rest = class:create("rest")
 
-function rest.public:get(route, timeout)
+function rest.public:get(route, timeout, headers, credentials)
     if self ~= rest.public then return false end
     if not route or (imports.type(route) ~= "string") then return false end
     timeout = math.max(imports.tonumber(timeout) or 10000, 1)
     local cPromise = thread:createPromise()
-    imports.fetchRemote(route, {queueName = "assetify_library", connectionAttempts = 1, connectTimeout = timeout, method = "GET"}, function(result, status)
+    imports.fetchRemote(route, {queueName = "assetify_library", connectionAttempts = 1, connectTimeout = timeout, method = "GET", headers = headers, username = (credentials and credentials.username) or nil, password = (credentials and credentials.password) or nil}, function(result, status)
+        print(result, status.success)
         if status.success then cPromise.resolve(result)
         else cPromise.reject(false, status.statusCode) end
     end)
     return cPromise
 end
 
-function rest.public:post(route, data, timeout, credentials)
+function rest.public:post(route, data, timeout, headers, credentials)
     if (self ~= rest.public) or not data then return false end
     if not route or (imports.type(route) ~= "string") then return false end
     timeout = math.max(imports.tonumber(timeout) or 10000, 1)
+    headers = ((imports.type(headers) == "table") and headers) or nil
     credentials = ((imports.type(credentials) == "table") and credentials) or false
     local cPromise = thread:createPromise()
-    imports.fetchRemote(route, {queueName = "assetify_library", connectionAttempts = 1, connectTimeout = timeout, method = "POST", username = (credentials and credentials.username) or nil, password = (credentials and credentials.password) or nil, formFields = data}, function(result, status)
+    imports.fetchRemote(route, {queueName = "assetify_library", connectionAttempts = 1, connectTimeout = timeout, method = "POST", headers = headers, username = (credentials and credentials.username) or nil, password = (credentials and credentials.password) or nil, formFields = data}, function(result, status)
         if status.success then cPromise.resolve(result)
         else cPromise.reject(false, status.statusCode) end
     end)
