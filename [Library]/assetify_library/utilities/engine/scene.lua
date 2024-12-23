3b3762ab1cc73516e572cd7c22ad9d246c6b4a29
Tron <89867985+ov-aviril@users.noreply.github.com>
2024-12-23 04:58:33 +0300

    Improved ipl & ide parser

--- [Library]/assetify_library/utilities/engine/scene.lua
+++ [Library]/assetify_library/utilities/engine/scene.lua
@@ -34,7 +34,6 @@ local imports = {
 
 local scene = class:create("scene")
 scene.private.cache = {col = {}}
-scene.private.separators = {IPL = ", ", IDE = ", "}
 scene.private.native = {
     buffer = imports.loadstring(file:read("utilities/rw/native/buffer.rw"))(),
     buffer_lod = imports.loadstring(file:read("utilities/rw/native/buffer_lod.rw"))()
@@ -48,7 +47,7 @@ function scene.private:fetchNativeLOD(modelName)
     return scene.private.native.buffer_lod[modelName] or false
 end
 
-function scene.private:createObject(...)
+function scene.private:createEntity(...)
     return ((params[9] and imports.createBuilding) or imports.createObject)(table.unpack(params, (params[9] and 7) or 8))
 end
 
@@ -57,11 +56,13 @@ function scene.public:parseIDE(rw)
     if not rw then return false end
     local result = {}
     for i = 1, table.length(rw), 1 do
-        local data = string.split(rw[i], scene.private.separators.IDE)
-        data[2] = (data[2] and string.gsub(data[2], "%s", "")) or data[2]
+        local data = string.split(rw[i], ",")
+        for k = 1, table.length(data), 1 do
+            data[k] = string.gsub(data[k], "%s", "")
+        end
         if data[2] then
             result[(data[2])] = {
-                (data[3] and string.gsub(data[3], "%s", "")) or false,
+                data[3] or false,
                 imports.tonumber(data[4])
             }
         end
@@ -74,8 +75,10 @@ function scene.public:parseIPL(rw, isNativeModelsEnabled)
     if not rw then return false end
     local result = {}
     for i = 1, table.length(rw), 1 do
-        local data = string.split(rw[i], scene.private.separators.IPL)
-        data[2] = (data[2] and string.gsub(data[2], "%s", "")) or data[2]
+        local data = string.split(rw[i], ",")
+        for k = 1, table.length(data), 1 do
+            data[k] = string.gsub(data[k], "%s", "")
+        end
         if data[2] then
             data.nativeID = (isNativeModelsEnabled and scene.private:fetchNativeID(data[2])) or nil
             data.nativeLOD = (data.nativeID and scene.private:fetchNativeLOD(data[2])) or nil
@@ -104,20 +107,20 @@ if localPlayer then
         if not scene.public:isInstance(self) then return false end
         if not cAsset or (not cAsset.nativeID and not cAsset.synced) or not sceneManifest or not sceneData then return false end
         local posX, posY, posZ, rotX, rotY, rotZ = sceneData.position.x + ((sceneManifest.sceneOffsets and sceneManifest.sceneOffsets.x) or 0), sceneData.position.y + ((sceneManifest.sceneOffsets and sceneManifest.sceneOffsets.y) or 0), sceneData.position.z + ((sceneManifest.sceneOffsets and sceneManifest.sceneOffsets.z) or 0), sceneData.rotation.x, sceneData.rotation.y, sceneData.rotation.z
-        self.cStreamerInstance = scene.private:createObject(cAsset.nativeID or cAsset.synced.modelID, posX, posY, posZ, rotX, rotY, rotZ, (sceneManifest.sceneLODs and not cAsset.nativeID and not cAsset.synced.lodID and cAsset.synced.collisionID and true) or false, sceneManifest.sceneBuildings) or false
+        self.cStreamerInstance = scene.private:createEntity(cAsset.nativeID or cAsset.synced.modelID, posX, posY, posZ, rotX, rotY, rotZ, (sceneManifest.sceneLODs and not cAsset.nativeID and not cAsset.synced.lodID and cAsset.synced.collisionID and true) or false, sceneManifest.sceneBuildings) or false
         if not self.cStreamerInstance then return false end
         imports.setElementDoubleSided(self.cStreamerInstance, sceneManifest.sceneDoublesided)
         if not cAsset.nativeID then
             imports.setElementCollisionsEnabled(self.cStreamerInstance, false)
-            self.cCollisionInstance = (cAsset.synced.collisionID and scene.private:createObject(cAsset.synced.collisionID, posX, posY, posZ, rotX, rotY, rotZ, false, sceneManifest.sceneBuildings)) or false
+            self.cCollisionInstance = (cAsset.synced.collisionID and scene.private:createEntity(cAsset.synced.collisionID, posX, posY, posZ, rotX, rotY, rotZ, false, sceneManifest.sceneBuildings)) or false
             if self.cCollisionInstance then
                 imports.setElementAlpha(self.cCollisionInstance, 0)
                 imports.setElementDimension(self.cCollisionInstance, sceneManifest.sceneDimension)
                 imports.setElementInterior(self.cCollisionInstance, sceneManifest.sceneInterior)
                 imports.setElementCollisionsEnabled(self.cCollisionInstance, false)
                 if sceneManifest.sceneLODs then
-                    self.cModelInstance = scene.private:createObject(cAsset.synced.collisionID, posX, posY, posZ, rotX, rotY, rotZ, true, sceneManifest.sceneBuildings) or false
-                    self.cLODInstance = (cAsset.synced.lodID and scene.private:createObject(cAsset.synced.lodID, posX, posY, posZ, rotX, rotY, rotZ, true, sceneManifest.sceneBuildings)) or false
+                    self.cModelInstance = scene.private:createEntity(cAsset.synced.collisionID, posX, posY, posZ, rotX, rotY, rotZ, true, sceneManifest.sceneBuildings) or false
+                    self.cLODInstance = (cAsset.synced.lodID and scene.private:createEntity(cAsset.synced.lodID, posX, posY, posZ, rotX, rotY, rotZ, true, sceneManifest.sceneBuildings)) or false
                     attacher:attachElements(self.cModelInstance, self.cCollisionInstance)
                     imports.setElementAlpha(self.cModelInstance, 0)
                     imports.setElementDimension(self.cModelInstance, sceneManifest.sceneDimension)
@@ -135,7 +138,7 @@ if localPlayer then
                 end
             end
         else
-            self.cLODInstance = (sceneManifest.sceneLODs and scene.private:createObject(cAsset.nativeLOD or cAsset.nativeID, posX, posY, posZ, rotX, rotY, rotZ, true, sceneManifest.sceneBuildings)) or false
+            self.cLODInstance = (sceneManifest.sceneLODs and scene.private:createEntity(cAsset.nativeLOD or cAsset.nativeID, posX, posY, posZ, rotX, rotY, rotZ, true, sceneManifest.sceneBuildings)) or false
             self.cCollisionInstance = self.cStreamerInstance
             if self.cLODInstance then
                 imports.setElementDoubleSided(self.cLODInstance, sceneManifest.sceneDoublesided)
