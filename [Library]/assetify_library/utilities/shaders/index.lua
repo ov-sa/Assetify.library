cbdccd15b60e7c317806c1703fcdaa2312af0113
Tron <89867985+ov-aviril@users.noreply.github.com>
2024-12-23 05:12:43 +0300

    Update index.lua

--- [Library]/assetify_library/utilities/shaders/index.lua
+++ [Library]/assetify_library/utilities/shaders/index.lua
@@ -139,6 +139,19 @@ function shaderRW.create(rtModes, isDepthMode)
             string skipUnusedParameters = "yes";
         >;
 
+        float2 hash2DStochastic(float2 s) { return frac(sin(fmod(float2(dot(s, float2(127.1, 311.7)), dot(s, float2(269.5, 183.3))), 3.14159))*43758.5453); }
+        float4 tex2DStochastic(sampler2D tex, float2 uv) {
+            float4x3 BW_vx;
+            float2 skewUV = mul(float2x2 (1.0, 0.0, -0.57735027, 1.15470054), uv*3.464);
+            float2 vxID = float2(floor(skewUV));
+            float3 barry = float3(frac(skewUV), 0);
+            barry.z = 1.0 - barry.x - barry.y;
+            BW_vx = barry.z > 0 ? float4x3(float3(vxID, 0), float3(vxID + float2(0, 1), 0), float3(vxID + float2(1, 0), 0), barry.zyx) : float4x3(float3(vxID + float2(1, 1), 0), float3(vxID + float2(1, 0), 0), float3(vxID + float2(0, 1), 0), float3(-barry.z, 1.0 - barry.y, 1.0 - barry.x));
+            float2 dx = ddx(uv);
+            float2 dy = ddy(uv);
+            return mul(tex2D(tex, uv + hash2DStochastic(BW_vx[0].xy), dx, dy), BW_vx[3].x) + mul(tex2D(tex, uv + hash2DStochastic(BW_vx[1].xy), dx, dy), BW_vx[3].y) + mul(tex2D(tex, uv + hash2DStochastic(BW_vx[2].xy), dx, dy), BW_vx[3].z);
+        }
+
         float MTAUnlerp(float from, float to, float pos) {
             if (from == to) return 1;
             else return (pos - from) / (to - from);
