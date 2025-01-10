d1f4c37ac84952cb8db1060ea6b8fdf64c723d42
Tron <89867985+ov-aviril@users.noreply.github.com>
2025-01-10 10:49:15 +0300

    Update tex_mapper.lua

--- [Library]/assetify_library/utilities/shaders/tex_mapper.lua
+++ [Library]/assetify_library/utilities/shaders/tex_mapper.lua
@@ -15,24 +15,26 @@
 local identity = "Assetify_TextureMapper"
 local iteration = 3
 shaderRW.buffer[identity] = {
-    exec = function(shaderMaps)
-        if not shaderMaps or (table.length(shaderMaps) <= 0) then return false end
-        local isSamplingStage = false
-        local controlVars, handlerBody, handlerFooter = [[
-            sampler baseSampler = sampler_state {
-                Texture = gTexture0;
-                MipFilter = Linear;
-                MaxAnisotropy = gMaxAnisotropy*anisotropy;
-                MinFilter = Anisotropic;
-            };
-        ]], "", ""
-        handlerBody = handlerBody..[[
-            float4 baseTexel = tex2D(baseSampler, PS.TexCoord);
-        ]]
+    prepare = function(shaderMaps, shaderModel)
+        local sampled = false
+        local result = {
+            config = [[
+                sampler baseSampler = sampler_state {
+                    Texture = gTexture0;
+                    MipFilter = Linear;
+                    MaxAnisotropy = gMaxAnisotropy*anisotropy;
+                    MinFilter = Anisotropic;
+                };
+            ]],
+            body = [[
+                float4 baseTexel = tex2D(baseSampler, PS.TexCoord);
+            ]],
+            footer = [[]]
+        }
         for i = table.length(shaderMaps), 1, -1 do
             local j = shaderMaps[i]
             if j.control then
-                controlVars = controlVars..[[
+                result.config = result.config..[[
                     texture controlTex_]]..i..[[;
                     sampler controlSampler_]]..i..[[ = sampler_state { 
                         Texture = controlTex_]]..i..[[;
@@ -42,12 +44,12 @@ shaderRW.buffer[identity] = {
                     };
                 ]]
             end
-            handlerBody = handlerBody..[[
+            result.body = result.body..[[
                 float4 controlTexel_]]..i..[[ = ]]..(((j.control) and [[tex2D(controlSampler_]]..i..[[, PS.TexCoord)]]) or [[baseTexel]])..[[;
                 float4 sampledTexel_]]..i..[[ = controlTexel_]]..i..[[;
             ]]
             if j.bump then
-                controlVars = controlVars..[[
+                result.config = result.config..[[
                     texture controlTex_]]..i..[[_bump;
                     sampler controlSampler_]]..i..[[_bump = sampler_state { 
                         Texture = controlTex_]]..i..[[_bump;
@@ -56,14 +58,14 @@ shaderRW.buffer[identity] = {
                         MipFilter = Linear;
                     };
                 ]]
-                handlerBody = handlerBody..[[
+                result.body = result.body..[[
                     float4 controlTexel_]]..i..[[_bump = tex2D(controlSampler_]]..i..[[_bump, PS.TexCoord);
                 ]]
             end
             for k = 1, table.length(shader.validChannels), 1 do
                 local v, channel = shader.validChannels[k].index, shader.validChannels[k].channel
                 if j[v] then
-                    controlVars = controlVars..[[
+                    result.config = result.config..[[
                         texture controlTex_]]..i..[[_]]..v..[[;
                         float controlScale_]]..i..[[_]]..v..[[ = ]]..(j[v].scale)..[[;
                         sampler controlSampler_]]..i..[[_]]..v..[[ = sampler_state { 
@@ -73,17 +75,17 @@ shaderRW.buffer[identity] = {
                             MinFilter = Anisotropic;
                         };
                     ]]
-                    if not j[v].stochastic then
-                        handlerBody = handlerBody..[[
+                    if (shaderModel < 3) or not j[v].stochastic then
+                        result.body = result.body..[[
                             float4 controlTexel_]]..i..[[_]]..v..[[ = tex2D(controlSampler_]]..i..[[_]]..v..[[, PS.TexCoord*controlScale_]]..i..[[_]]..v..[[);
                         ]]
                     else
-                        handlerBody = handlerBody..[[
+                        result.body = result.body..[[
                             float4 controlTexel_]]..i..[[_]]..v..[[ = tex2DStochastic(controlSampler_]]..i..[[_]]..v..[[, PS.TexCoord*controlScale_]]..i..[[_]]..v..[[);
                         ]]
                     end
                     if j[v].bump then
-                        controlVars = controlVars..[[
+                        result.config = result.config..[[
                             texture controlTex_]]..i..[[_]]..v..[[_bump;
                             sampler controlSampler_]]..i..[[_]]..v..[[_bump = sampler_state { 
                                 Texture = controlTex_]]..i..[[_]]..v..[[_bump;
@@ -92,53 +94,59 @@ shaderRW.buffer[identity] = {
                                 MipFilter = Linear;
                             };
                         ]]
-                        if not j[v].stochastic then
-                            handlerBody = handlerBody..[[
+                        if (shaderModel < 3) or not j[v].stochastic then
+                            result.body = result.body..[[
                                 float4 controlTexel_]]..i..[[_]]..v..[[_bump = tex2D(controlSampler_]]..i..[[_]]..v..[[_bump, PS.TexCoord*controlScale_]]..i..[[_]]..v..[[);
                             ]]
                         else
-                            handlerBody = handlerBody..[[
+                            result.body = result.body..[[
                                 float4 controlTexel_]]..i..[[_]]..v..[[_bump = tex2DStochastic(controlSampler_]]..i..[[_]]..v..[[_bump, PS.TexCoord*controlScale_]]..i..[[_]]..v..[[);
                             ]]
                         end
                     end
                     for m = 1, iteration, 1 do
-                        handlerBody = handlerBody..[[
+                        result.body = result.body..[[
                             sampledTexel_]]..i..[[ = lerp(sampledTexel_]]..i..[[, controlTexel_]]..i..[[_]]..v..[[, controlTexel_]]..i..[[.]]..channel..[[);
                         ]]
                     end
                     if j[v].bump then
-                        handlerBody = handlerBody..[[
+                        result.body = result.body..[[
                             sampledTexel_]]..i..[[.rgb *= controlTexel_]]..i..[[_]]..v..[[_bump.rgb;
                         ]]
                     end
                 end
             end
-            handlerBody = handlerBody..[[
+            result.body = result.body..[[
                 sampledTexel_]]..i..[[.rgb *= ]]..(1/iteration)..[[;
             ]]
             if j.bump then
-                handlerBody = handlerBody..[[
+                result.body = result.body..[[
                     sampledTexel_]]..i..[[.rgb *= controlTexel_]]..i..[[_bump.rgb;
                 ]]
             end
-            handlerBody = handlerBody..[[
+            result.body = result.body..[[
                 sampledTexel_]]..i..[[.a = controlTexel_]]..i..[[.a;
             ]]
-            handlerFooter = handlerFooter..((not isSamplingStage and [[
+            result.footer = result.footer..((not sampled and [[
                 float4 sampledTexel = sampledTexel_]]..i..[[;
             ]]) or [[
                 sampledTexel = lerp(sampledTexel, sampledTexel_]]..i..[[, sampledTexel_]]..i..[[.a);
             ]])
-            isSamplingStage = true
+            sampled = true
         end
+        return result
+    end,
+
+    exec = function(shaderMaps)
+        if not shaderMaps or (table.length(shaderMaps) <= 0) then return false end
+        local query = shaderRW.buffer[identity].prepare(shaderMaps, 3)
         return shaderRW.create({diffuse = true, emissive = true})..[[
         /*-----------------
         -->> Variables <<--
         -------------------*/
 
         float anisotropy = 1;
-        ]]..controlVars..[[
+        ]]..query.config..[[
         struct VSInput {
             float3 Position : POSITION0;
             float2 TexCoord : TEXCOORD0;
@@ -170,7 +178,7 @@ shaderRW.buffer[identity] = {
     
         Export PSHandler(PSInput PS) : COLOR0 {
             Export output;
-            ]]..handlerBody..handlerFooter..[[
+            ]]..query.body..query.footer..[[
             if (vRenderingEnabled) {
                 output.Diffuse = vEmissiveSource ? 0 : sampledTexel;
                 output.Emissive = vEmissiveSource ? sampledTexel : 0;
