020a977ace24527ec7f6caaa8f9be088ca9f51f3
Tron <103286142+ov-anisa@users.noreply.github.com>
2025-03-05 03:51:18 +0300

    Made API handlers more compact and readable

--- [Library]/assetify_library/handlers/api/library.lua
+++ [Library]/assetify_library/handlers/api/library.lua
@@ -12,46 +12,16 @@
 --[[ APIs: Library ]]--
 -----------------------
 
-manager:exportAPI("library", "isBooted", function()
-    return syncer.isLibraryBooted
-end)
-
-manager:exportAPI("library", "isLoaded", function()
-    return syncer.isLibraryLoaded
-end)
-
-manager:exportAPI("library", "isModuleLoaded ", function()
-    return syncer.isModuleLoaded
-end)
-
-manager:exportAPI("library", "fetchSerial", function()
-    return syncer.librarySerial
-end)
-
-manager:exportAPI("library", "fetchVersion", function()
-    return syncer.libraryVersion
-end)
-
-manager:exportAPI("library", "fetchWebserver", function()
-    return syncer.libraryWebserver
-end)
-
-manager:exportAPI("library", "fetchAssets", function(...)
-    return manager:fetchAssets(...)
-end)
-
-manager:exportAPI("library", "getAssetData", function(...)
-    return manager:getAssetData(...)
-end)
-
-manager:exportAPI("library", "getAssetDep", function(...)
-    return manager:getAssetDep(...)
-end)
-
-manager:exportAPI("library", "setElementAsset", function(...)
-    return syncer.syncElementModel(_, ...)
-end)
-
+manager:exportAPI("library", "isBooted", function() return syncer.isLibraryBooted end)
+manager:exportAPI("library", "isLoaded", function() return syncer.isLibraryLoaded end)
+manager:exportAPI("library", "isModuleLoaded ", function() return syncer.isModuleLoaded end)
+manager:exportAPI("library", "fetchSerial", function() return syncer.librarySerial end)
+manager:exportAPI("library", "fetchVersion", function() return syncer.libraryVersion end)
+manager:exportAPI("library", "fetchWebserver", function() return syncer.libraryWebserver end)
+manager:exportAPI("library", "fetchAssets", function(...) return manager:fetchAssets(...) end)
+manager:exportAPI("library", "getAssetData", function(...) return manager:getAssetData(...) end)
+manager:exportAPI("library", "getAssetDep", function(...) return manager:getAssetDep(...) end)
+manager:exportAPI("library", "setElementAsset", function(...) return syncer.syncElementModel(_, ...) end)
 manager:exportAPI("library", "getElementAsset", function(element)
     if not syncer.syncedElements[element] then return false end
     return syncer.syncedElements[element].assetType, syncer.syncedElements[element].assetName, syncer.syncedElements[element].assetClump, syncer.syncedElements[element].clumpMaps
@@ -63,25 +33,11 @@ manager:exportAPI("library", "createDummy", function(...)
 end)
 
 if localPlayer then
-    manager:exportAPI("library", "getDownloadProgress", function(...)
-        return manager:getDownloadProgress(...)
-    end)
-
-    manager:exportAPI("library", "isAssetLoaded", function(...)
-        return manager:isAssetLoaded(...)
-    end)
-
-    manager:exportAPI("library", "getAssetID", function(...)
-        return manager:getAssetID(...)
-    end)
-
-    manager:exportAPI("library", "loadAsset", function(...)
-        return manager:loadAsset(...)
-    end)
-
-    manager:exportAPI("library", "unloadAsset", function(...)
-        return manager:unloadAsset(...)
-    end)
+    manager:exportAPI("library", "getDownloadProgress", function(...) return manager:getDownloadProgress(...) end)
+    manager:exportAPI("library", "isAssetLoaded", function(...) return manager:isAssetLoaded(...) end)
+    manager:exportAPI("library", "getAssetID", function(...) return manager:getAssetID(...) end)
+    manager:exportAPI("library", "loadAsset", function(...) return manager:loadAsset(...) end)
+    manager:exportAPI("library", "unloadAsset", function(...) return manager:unloadAsset(...) end)
     
     manager:exportAPI("library", "createShader", function(...)
         local cShader = shader:create(...)
--- [Library]/assetify_library/handlers/api/renderer.lua
+++ [Library]/assetify_library/handlers/api/renderer.lua
@@ -13,109 +13,32 @@
 ------------------------
 
 if localPlayer then
-    manager:exportAPI("renderer", "isVirtualRendering", function()
-        return renderer.isVirtualRendering
-    end)
-
-    manager:exportAPI("renderer", "setVirtualRendering", function(...)
-        return renderer:setVirtualRendering(...)
-    end)
-
-    manager:exportAPI("renderer", "getVirtualSource", function()
-        return (renderer.isVirtualRendering and renderer.virtualSource) or false
-    end)
-
-    manager:exportAPI("renderer", "getVirtualRTs", function()
-        return (renderer.isVirtualRendering and renderer.virtualRTs) or false
-    end)
-
-    manager:exportAPI("renderer", "isTimeSynced", function(...)
-        return renderer.isTimeSynced
-    end)
-
-    manager:exportAPI("renderer", "setTimeSync", function(...)
-        return renderer:setTimeSync(...)
-    end)
-
-    manager:exportAPI("renderer", "setServerTick", function(...)
-        return renderer:setServerTick(...)
-    end)
-
-    manager:exportAPI("renderer", "setMinuteDuration", function(...)
-        return renderer:setMinuteDuration(...)
-    end)
-
-    manager:exportAPI("renderer", "isEmissiveMode", function()
-        return renderer.isEmissiveModeEnabled
-    end)
-
-    manager:exportAPI("renderer", "setEmissiveMode", function(...)
-        return renderer:setEmissiveMode(...)
-    end)
-
-    manager:exportAPI("renderer", "isDynamicSky", function()
-        return renderer.isDynamicSkyEnabled
-    end)
-
-    manager:exportAPI("renderer", "setDynamicSky", function(...)
-        return renderer:setDynamicSky(...)
-    end)
-
-    manager:exportAPI("renderer", "isDynamicPrelights", function()
-        return renderer.isDynamicPrelightsEnabled
-    end)
-
-    manager:exportAPI("renderer", "setDynamicPrelights", function(...)
-        return renderer:setDynamicPrelights(...)
-    end)
-
-    manager:exportAPI("renderer", "getDynamicSunColor", function()
-        return renderer.isDynamicSunColor[1]*255, renderer.isDynamicSunColor[2]*255, renderer.isDynamicSunColor[3]*255
-    end)
-
-    manager:exportAPI("renderer", "setDynamicSunColor", function(...)
-        return renderer:setDynamicSunColor(...)
-    end)
-
-    manager:exportAPI("renderer", "isDynamicStars", function()
-        return renderer.isDynamicStarsEnabled
-    end)
-
-    manager:exportAPI("renderer", "setDynamicStars", function(...)
-        return renderer:setDynamicStars(...)
-    end)
-
-    manager:exportAPI("renderer", "getDynamicCloudDensity", function()
-        return renderer.isDynamicCloudDensity
-    end)
-
-    manager:exportAPI("renderer", "setDynamicCloudDensity", function(...)
-        return renderer:setDynamicCloudDensity(...)
-    end)
-
-    manager:exportAPI("renderer", "getDynamicCloudScale", function()
-        return renderer.isDynamicCloudScale
-    end)
-
-    manager:exportAPI("renderer", "setDynamicCloudScale", function(...)
-        return renderer:setDynamicCloudScale(...)
-    end)
-
-    manager:exportAPI("renderer", "getDynamicCloudColor", function()
-        return renderer.isDynamicCloudColor[1]*255, renderer.isDynamicCloudColor[2]*255, renderer.isDynamicCloudColor[3]*255
-    end)
-
-    manager:exportAPI("renderer", "setDynamicCloudColor", function(...)
-        return renderer:setDynamicCloudColor(...)
-    end)
-
-    manager:exportAPI("renderer", "getTimeCycle", function()
-        return renderer.isDynamicTimeCycle
-    end)
-
-    manager:exportAPI("renderer", "setTimeCycle", function(...)
-        return renderer:setTimeCycle(...)
-    end)
+    manager:exportAPI("renderer", "isVirtualRendering", function() return renderer.isVirtualRendering end)
+    manager:exportAPI("renderer", "setVirtualRendering", function(...) return renderer:setVirtualRendering(...) end)
+    manager:exportAPI("renderer", "getVirtualSource", function() return (renderer.isVirtualRendering and renderer.virtualSource) or false end)
+    manager:exportAPI("renderer", "getVirtualRTs", function() return (renderer.isVirtualRendering and renderer.virtualRTs) or false end)
+    manager:exportAPI("renderer", "isTimeSynced", function(...) return renderer.isTimeSynced end)
+    manager:exportAPI("renderer", "setTimeSync", function(...) return renderer:setTimeSync(...) end)
+    manager:exportAPI("renderer", "setServerTick", function(...) return renderer:setServerTick(...) end)
+    manager:exportAPI("renderer", "setMinuteDuration", function(...) return renderer:setMinuteDuration(...) end)
+    manager:exportAPI("renderer", "isEmissiveMode", function() return renderer.isEmissiveModeEnabled end)
+    manager:exportAPI("renderer", "setEmissiveMode", function(...) return renderer:setEmissiveMode(...) end)
+    manager:exportAPI("renderer", "isDynamicSky", function() return renderer.isDynamicSkyEnabled end)
+    manager:exportAPI("renderer", "setDynamicSky", function(...) return renderer:setDynamicSky(...) end)
+    manager:exportAPI("renderer", "isDynamicPrelights", function() return renderer.isDynamicPrelightsEnabled end)
+    manager:exportAPI("renderer", "setDynamicPrelights", function(...) return renderer:setDynamicPrelights(...) end)
+    manager:exportAPI("renderer", "getDynamicSunColor", function() return renderer.isDynamicSunColor[1]*255, renderer.isDynamicSunColor[2]*255, renderer.isDynamicSunColor[3]*255 end)
+    manager:exportAPI("renderer", "setDynamicSunColor", function(...) return renderer:setDynamicSunColor(...) end)
+    manager:exportAPI("renderer", "isDynamicStars", function() return renderer.isDynamicStarsEnabled end)
+    manager:exportAPI("renderer", "setDynamicStars", function(...) return renderer:setDynamicStars(...) end)
+    manager:exportAPI("renderer", "getDynamicCloudDensity", function() return renderer.isDynamicCloudDensity end)
+    manager:exportAPI("renderer", "setDynamicCloudDensity", function(...) return renderer:setDynamicCloudDensity(...) end)
+    manager:exportAPI("renderer", "getDynamicCloudScale", function() return renderer.isDynamicCloudScale end)
+    manager:exportAPI("renderer", "setDynamicCloudScale", function(...) return renderer:setDynamicCloudScale(...) end)
+    manager:exportAPI("renderer", "getDynamicCloudColor", function() return renderer.isDynamicCloudColor[1]*255, renderer.isDynamicCloudColor[2]*255, renderer.isDynamicCloudColor[3]*255 end)
+    manager:exportAPI("renderer", "setDynamicCloudColor", function(...) return renderer:setDynamicCloudColor(...) end)
+    manager:exportAPI("renderer", "getTimeCycle", function() return renderer.isDynamicTimeCycle end)
+    manager:exportAPI("renderer", "setTimeCycle", function(...) return renderer:setTimeCycle(...) end)
 else
 
 end
