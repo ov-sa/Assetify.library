dc3c4c2b9ef2a41228c165277086549657035cdc
A <103286142+ov-anisa@users.noreply.github.com>
2025-04-06 22:53:59 +0300

    Assetify `v.11.1.5`

--- [Library]/assetify_library/handlers/api/syncer.lua
+++ [Library]/assetify_library/handlers/api/syncer.lua
@@ -12,13 +12,9 @@
 --[[ APIs: Syncer ]]--
 ----------------------
 
-if localPlayer then
-    manager:exportAPI("syncer", "setGlobalData", function(...) return syncer.syncGlobalData(...) end)
-    manager:exportAPI("syncer", "getGlobalData", function(data) return syncer.syncedGlobalDatas[data] end)
-    manager:exportAPI("syncer", "getAllGlobalDatas", function() return syncer.syncedGlobalDatas end)
-    manager:exportAPI("syncer", "setEntityData", function(...) return syncer.syncEntityData(table.unpack(table.pack(...), 3)) end)
-    manager:exportAPI("syncer", "getEntityData", function(element, data) return syncer.syncedEntityDatas[element] and syncer.syncedEntityDatas[element][data] end)
-    manager:exportAPI("syncer", "getAllEntityDatas", function(element) return syncer.syncedEntityDatas[element] or {} end)
-else
-
-end
+manager:exportAPI("syncer", "setGlobalData", function(...) return syncer.syncGlobalData(...) end)
+manager:exportAPI("syncer", "getGlobalData", function(data) return syncer.syncedGlobalDatas[data] end)
+manager:exportAPI("syncer", "getAllGlobalDatas", function() return syncer.syncedGlobalDatas end)
+manager:exportAPI("syncer", "setEntityData", function(...) return syncer.syncEntityData(table.unpack(table.pack(...), 3)) end)
+manager:exportAPI("syncer", "getEntityData", function(element, data) return syncer.syncedEntityDatas[element] and syncer.syncedEntityDatas[element][data] end)
+manager:exportAPI("syncer", "getAllEntityDatas", function(element) return syncer.syncedEntityDatas[element] or {} end)
